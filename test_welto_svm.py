dd9110eb9593c17eeee4164f2edf8ae2fd0a10d0
welto <welto@pharow.com>
2024-05-22 23:28:26 +0100

    - adjusting preprocessing steps - effect of random_state (train_test_split) on f1 and accuracy scores

--- test_welto_svm.py
+++ test_welto_svm.py
@@ -6,12 +6,24 @@ import numpy as np
 import pandas as pd
 import spacy
 
+import warnings
+import time
+
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
+
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = seconds % 60
+    return hours, minutes, secs
+
+# Ignore warnings
+warnings.filterwarnings("ignore")
 
 # Importation des données
 X_train = pd.read_csv(
@@ -25,6 +37,8 @@ Y_train = pd.read_csv(
 )
 
 # Preprocessing
+fast_coeff = 5
+
 # Suppression des colonnes inutiles
 X_train = X_train.drop(
     labels=['Unnamed: 0', 'imageid', 'description']
@@ -39,17 +53,20 @@ Y_train = Y_train.drop(
 X_train, X_test = train_test_split(
     X_train,
     test_size=0.2,
-    random_state=42
+    random_state=70
 )
 Y_train, Y_test = train_test_split(
     Y_train,
     test_size=0.2,
-    random_state=42
+    random_state=70
 )
 
 # Tokenisation et cleaning
-X_train_raw_designation = X_train['designation'].tolist()
-X_test_raw_designation = X_test['designation'].tolist()
+X_train_raw_designation = X_train['designation'][:X_train.shape[0]//fast_coeff].tolist()
+X_test_raw_designation = X_test['designation'][:X_test.shape[0]//fast_coeff].tolist()
+Y_train = Y_train['prdtypecode'][:Y_train.shape[0]//fast_coeff].tolist()
+Y_test = Y_test['prdtypecode'][:Y_test.shape[0]//fast_coeff].tolist()
+
 
 spacy_nlp = spacy.load("fr_core_news_sm")
 
@@ -66,6 +83,8 @@ header.progress_bar(
     suffix='Complete',
     length=50
 )
+
+preprocessing_start_time = time.time()
 
 for k in range(a):
     X_train_raw_designation_clean.append(
@@ -97,29 +116,41 @@ for k in range(b):
         length=50
     )
 
+preprocessing_end_time = time.time()
+preprocessing_time_h, preprocessing_time_min, preprocessing_time_s = convert_seconds(preprocessing_end_time - preprocessing_start_time)
+
+print(f"Preprocessed in {int(preprocessing_time_h)}h {int(preprocessing_time_min)}min {int(preprocessing_time_s)}s")
+
 # Vectorisation
 tfidf = TfidfVectorizer()
 
 X_train_tfidf = tfidf.fit_transform(X_train_raw_designation_clean)
 X_test_tfidf = tfidf.transform(X_test_raw_designation_clean)
 
-# Entraînement
-fast_coeff = 1
+print(X_train_tfidf.shape, X_test_tfidf.shape)
 
-svm = SVC()
+# Entraînement
+svm = SVC(kernel='rbf')
+
+training_start_time = time.time()
 
 svm.fit(
-    X_train_tfidf[:X_train_tfidf.shape[0]//fast_coeff],
-    Y_train[:len(Y_train)//fast_coeff]
+    X_train_tfidf,
+    Y_train
 )
 
-Y_pred_svm = svm.predict(X_test_tfidf[:X_test_tfidf.shape[0]//fast_coeff])
+training_end_time = time.time()
+training_time_h, training_time_min, training_time_s = convert_seconds(training_end_time - training_start_time)
+
+print(f"Model trained in {int(training_time_h)}h {int(training_time_min)}min {int(training_time_s)}s")
+
+Y_pred_svm = svm.predict(X_test_tfidf)
 
 # Évaluation du modèle
 print(
     "f1 score :",
     f1_score(
-        Y_test[:X_test_tfidf.shape[0]//fast_coeff],
+        Y_test,
         Y_pred_svm,
         average='macro'
     )
@@ -127,7 +158,7 @@ print(
 print(
     "accuracy score :",
     accuracy_score(
-        Y_test[:X_test_tfidf.shape[0]//fast_coeff],
+        Y_test,
         Y_pred_svm
     )
 )
