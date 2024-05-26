b5e4822c21930370ffee8ebd7214ac6f9d60d4c5
Chatail--Rigolleau Alexis <alexis.chatail-rgiolleau@student-cs.fr>
2024-05-26 23:02:35 +0100

    pre release e2r0 test regression 1

--- Zmain.py
+++ Zmain.py
@@ -130,7 +130,8 @@ def main():
         #NN
         label_encoder = LabelEncoder()
         y_train_encoded = label_encoder.fit_transform(y_data)
-
+        print("Must load all models in order to predict")
+        print("1 - Neural Network ")
         DoReload_nn = input("Reload neural network model - mandatory if nn_model.keras doesn't exist - (yes/no) ? : ")
 
         if DoReload_nn == 'no' and path.exists('./models/nn_model.keras'):
@@ -151,6 +152,8 @@ def main():
         Save_label_output(y_test_pred_nn, len(X_train), './output/output_nn.csv')
 
         #SVM
+        print("NN model loaded")
+        print("2 - SVM ")
         DoReload_svm = input("Reload SVM model - mandatory if svm_model.joblib doesn't exist - (yes/no) ? : ")
         if DoReload_svm == 'no' and path.exists('./models/svm_model.joblib'):
             print("Loading pre-trained SVM model...")
@@ -164,24 +167,12 @@ def main():
         y_test_pred_svm = best_model.predict(X_test_tfidf)
         Save_label_output(y_test_pred_svm, len(X_train), './output/output_svm.csv')
 
-        #kNN
-        DoReload_kNN = input("Reload kNN model - mandatory if knn_model.joblib doesn't exist - (yes/no) ? : ")
-        if DoReload_kNN == 'no' and path.exists('./models/knn_model.joblib'):
-            print("Loading pre-trained kNN model...")
-            best_model = load('./models/knn_model.joblib')
-
-        elif DoReload_kNN == 'yes':
-            best_model = train_knn(X_train_tfidf, y_data)
-            dump(best_model, './models/knn_model.joblib')
-        else:
-            return 0
-
-        y_test_pred_knn = best_model.predict(X_test_tfidf)
-        Save_label_output(y_test_pred_knn, len(X_train), './output/output_knn.csv')
-
         #RF
         from ZRF import train_rf
         from joblib import dump,load
+        
+        print("SVM model loaded")
+        print("3 - Random Forest")
         DoReload = input("Reload RF model - mandatory if rf_model.joblib doesn't exist - (yes/no) ? : ")
         
         if DoReload=='no' and path.exists('./models/rf_model.joblib'):
@@ -195,10 +186,10 @@ def main():
             return 0
         y_pred_rf = best_model.predict(X_test_tfidf)
         Save_label_output(y_pred_rf, len(X_train), './output/output_rf.csv')
-
+        print("All models loaded, predicting labels ...")
         #détermination des meilleurs labels
         Save_label_output(
-            weighted_vote_prediction(y_test_pred_nn, y_test_pred_svm, y_test_pred_knn,y_pred_rf),
+            weighted_vote_prediction(y_test_pred_nn, y_test_pred_svm,y_pred_rf),
             len(X_train),
             './output/output_weighted_vote.csv'
         )
