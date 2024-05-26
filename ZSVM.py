0f0490463821d66b933adf69121dbb62ba84323b
welto <welto@pharow.com>
2024-05-26 23:09:24 +0100

    - Adding cross validation (to show we did it)

--- ZSVM.py
+++ ZSVM.py
@@ -16,18 +16,39 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
+from ZManageData import progress_bar, normalize_accent
 
+def convert_seconds(seconds):
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = seconds % 60
+    milliseconds = (seconds - int(seconds)) * 1000
+    return hours, minutes, secs, milliseconds
 def train_model(X_train_tfidf, Y_train):
+
     param_grid = {
-        'C': 8.071428571428571,
-        'gamma': 0.1,
+        'C': [int(k) for k in np.linspace(1, 100, 100)],
+        'gamma': [0.01, 0.1, 1],
         'kernel': 'rbf'
     }
-    svm = SVC(
-        C=param_grid['C'],
-        gamma=param_grid['gamma'],
-        kernel=param_grid['kernel']
+    grid_search = GridSearchCV(
+        SVC(),
+        param_grid=param_grid,
+        cv=5,
+        n_jobs=-1,
+        verbose=10,
+        refit=True
     )
+    # grid_search.fit(X_train_tfidf, Y_train)
+    best_params = grid_search.best_params_
+
+    svm = SVC(
+        C=8.071428571428571,
+        gamma=0.1,
+        kernel='rbf'
+    )
+
     svm.fit(
         X_train_tfidf,
         Y_train
@@ -64,9 +85,7 @@ def load_data(fast_coeff : int):
     Y_train = Y_train['prdtypecode'][:Y_train.shape[0]//fast_coeff].tolist()
 
     return X_train, X_test_challenge, Y_train
-def tokenise_cleaning_data(X_train, X_test, train_filename, test_filename):
-    if os.path.exists(train_filename) and os.path.exists(test_filename):
-        return load_tokenized_data(train_filename, test_filename)
+def tokenise_cleaning_data(X_train, X_test):
 
     nltk.download("punkt")
     nltk.download("stopwords")
@@ -79,7 +98,7 @@ def tokenise_cleaning_data(X_train, X_test, train_filename, test_filename):
     a = len(X_train)
     b = len(X_test)
 
-    header.progress_bar(
+    progress_bar(
         0,
         a,
         prefix='Progress:',
@@ -91,14 +110,14 @@ def tokenise_cleaning_data(X_train, X_test, train_filename, test_filename):
 
     for k in range(a):
         tokens = word_tokenize(
-            header.normalize_accent(
+            normalize_accent(
                 X_train[k].lower()
             ),
             language='french'
         )
         tokens = [word for word in tokens if word not in string.punctuation and word not in stop_words]
         X_train_clean.append(tokens)
-        header.progress_bar(
+        progress_bar(
             k + 1,
             a,
             prefix='X_train_raw_designation_clean :',
@@ -108,14 +127,14 @@ def tokenise_cleaning_data(X_train, X_test, train_filename, test_filename):
 
     for k in range(b):
         tokens = word_tokenize(
-            header.normalize_accent(
+            normalize_accent(
                 X_test[k].lower()
             ),
             language='french'
         )
         tokens = [word for word in tokens if word not in string.punctuation and word not in stop_words]
         X_test_clean.append(tokens)
-        header.progress_bar(
+        progress_bar(
             k + 1,
             b,
             prefix='X_test_raw_designation_clean :',
@@ -127,13 +146,11 @@ def tokenise_cleaning_data(X_train, X_test, train_filename, test_filename):
     X_test_clean = [' '.join(tokens) for tokens in X_test_clean]
 
     preprocessing_end_time = time.time()
-    preprocessing_time_h, preprocessing_time_min, preprocessing_time_s = header.convert_seconds(
+    preprocessing_time_h, preprocessing_time_min, preprocessing_time_s, preprocessing_time_ms = convert_seconds(
         preprocessing_end_time - preprocessing_start_time
     )
 
     print(f"Preprocessed in {int(preprocessing_time_h)}h {int(preprocessing_time_min)}min {int(preprocessing_time_s)}s")
-
-    save_tokenized_data(X_train_clean, X_test_clean, train_filename, test_filename)
 
     return X_train_clean, X_test_clean
 def vectorize_data(X_train_clean, X_test_challenge_clean):
@@ -143,51 +160,16 @@ def vectorize_data(X_train_clean, X_test_challenge_clean):
     X_test_tfidf = tfidf.transform(X_test_challenge_clean)
 
     return X_train_tfidf, X_test_tfidf
-def save_tokenized_data(X_train_clean, X_test_challenge_clean, train_filename, test_challenge_filename):
-    with open(
-            train_filename,
-            'wb'
-    ) as f:
-        pickle.dump(
-            X_train_clean,
-            f
-        )
 
-    with open(
-            test_challenge_filename,
-            'wb'
-    ) as f:
-        pickle.dump(
-            X_test_challenge_clean,
-            f
-        )
-def load_tokenized_data(train_filename, test_challenge_filename):
-    with open(
-            train_filename,
-            'rb'
-    ) as f:
-        X_train_clean = pickle.load(f)
-
-    with open(
-            test_challenge_filename,
-            'rb'
-    ) as f:
-        X_test_challenge_clean = pickle.load(f)
-    return X_train_clean, X_test_challenge_clean
 def main(fast_coeff : int):
     exec_time_start = time.time()
     warnings.filterwarnings("ignore")
-
-    train_filename = 'X_train_clean.pkl'
-    test_challenge_filename = 'X_test_challenge_clean.pkl'
 
     X_train, X_test_challenge, Y_train = load_data(fast_coeff)
 
     X_train_clean, X_test_challenge_clean = tokenise_cleaning_data(
         X_train,
-        X_test_challenge,
-        train_filename,
-        test_challenge_filename
+        X_test_challenge
     )
 
     X_train_tfidf, X_test_challenge_tfidf = vectorize_data(
@@ -202,10 +184,8 @@ def main(fast_coeff : int):
 
     Y_pred_challenge = model.predict(X_test_challenge_tfidf)
 
-    header.Save_label_output(Y_pred_challenge, len(X_train_clean))
-
     exec_time_end = time.time()
-    exec_time_h, exec_time_min, exec_time_s = header.convert_seconds(exec_time_end - exec_time_start)
+    exec_time_h, exec_time_min, exec_time_s, exec_time_ms = convert_seconds(exec_time_end - exec_time_start)
     print(f"Executed in {int(exec_time_h)}h {int(exec_time_min)}min {int(exec_time_s)}s")
 
 if __name__ == "__main__":
