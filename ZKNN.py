ced5276e44918bb756bb58014cfc420f2b5a8113
welto <welto@pharow.com>
2024-05-26 15:49:48 +0100

    - Adding cross validation code

--- ZKNN.py
+++ ZKNN.py
@@ -4,6 +4,19 @@ from sklearn.model_selection import GridSearchCV
 
 
 def train_knn(X_train, y_train):
+    params = {'n_neighbors': range(2, 20)}
+    n_folds = 10
+    cv = KFold(n_splits=n_folds, shuffle=False)
+
+    grid_search = GridSearchCV(
+        estimator=KNeighborsClassifier(),
+        param_grid=params,
+        return_train_score=True,
+        cv=cv,
+    ).fit(X_train, y_train)
+
+    best_estimator = grid_search.best_estimator_
+
     BestKNN = KNeighborsClassifier(n_neighbors=49)
     BestKNN.fit(X_train, y_train)
 
