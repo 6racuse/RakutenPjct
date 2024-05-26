9bf33a66c5e0a982edefcc945563921aeabec51b
welto <welto@pharow.com>
2024-05-26 15:05:27 +0100

    - download_nltk

--- ZGlobal_parameter.py
+++ ZGlobal_parameter.py
@@ -9,5 +9,4 @@ class Model_Map(Enum):
     MODEL_NN   =   auto()  #Neural Network
     MODEL_SVM  =   auto()  #SVM model
     MODEL_KNN  =   auto()  #KNN model
-    MODEL_RESULTS = auto() #Solution to the project
     
--- test_result.py
+++ test_result.py
@@ -4,7 +4,9 @@ from pandas import read_csv
 nn = read_csv('./output/output_nn.csv',index_col=0)
 svm = read_csv('./output/output_svm.csv',index_col=0)
 y81 = read_csv('./output/ylabel_nn_0_8218.csv',index_col=0)
+knn = read_csv('./output/output_knn.csv', index_col=0)
 
 
 print("nn:",accuracy_score(y81,nn))
 print("svm:",accuracy_score(y81,svm))
+print("knn:", accuracy_score(y81,knn))
